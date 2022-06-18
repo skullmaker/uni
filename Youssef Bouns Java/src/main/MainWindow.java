50717cadb0cbc917b45d27519e27988f355ec6d8
skullmaker <107766805+skullmaker@users.noreply.github.com>
2022-06-18 19:05:12 +0200

    Add files via upload
    calculator Javafx

---
+++ Youssef Bouns Java/src/main/MainWindow.java
@@ -0,0 +1,28 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainWindow extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI.fxml"));
+        Scene scene = new Scene(loader.load());
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        stage.setTitle("Calculator");
+        ((MainWindowController)loader.getController()).init(stage);
+        stage.show();
+    }
+
+    public void run() {
+        launch();
+    }
+}
